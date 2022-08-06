6edd4c36a23e33c4eec1eea53346bcb5861f0771
AceJester90 <94662304+AceJester90@users.noreply.github.com>
2022-08-06 19:21:13 +0800

    Update mintTokens.sol

--- contracts/mintTokens.sol
+++ contracts/mintTokens.sol
@@ -8,6 +8,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 interface Iowner {
     function owner() external view returns (address);
     function ownerOf(uint id) external view;
+    function totalSupply(uint id) external view;
 }
 
 contract mintTokens is Ownable, ERC1155MultiUri{
@@ -17,16 +18,44 @@ contract mintTokens is Ownable, ERC1155MultiUri{
     function viewOwner(address _token, uint _id) public view{
         Iowner(_token).owner();
         Iowner(_token).ownerOf(_id);
+        Iowner(_token).totalSupply(_id);
     }
 
-    function mintuniquenft(address _account, uint256 _id, uint256 _amount, bytes memory data, string memory _newuri) public onlyOwner{
-        _amount = 1;
+    function viewOwner() public view{
+    }
+
+    function mintUniqueNft(address _account,//allow contract owner to mint copy to an address
+    uint256 _id,
+    bytes memory data,
+    string memory _newuri) public onlyOwner{
+        uint _amount = 1;
         _mintWithURI(_account, _id, _amount, data, _newuri);
     }
 
-    function mintcopy(address _account, uint256 _id, uint256 _amount, bytes memory data) public onlyOwner {
-        _amount = 1;
+    function mintCopy(address _account,//allow contract owner to mint copy to an address
+    uint256 _id,
+    bytes memory data) public onlyOwner {
+        uint _amount = 1;
         _mintWithoutURI(_account, _id, _amount, data);
+    }
+
+    function mintUniqueNftToMultipleAddress(address[] memory _accounts,//allow contract owner to mint new unique nft to multiple addresses
+    uint256 _id,
+    bytes memory data,
+    string memory _newuri) public onlyOwner{
+        for(uint i = 0; i < _accounts.length; i++){
+            uint _amount = 1;
+            _mintWithURI(_accounts[i], _id, _amount, data, _newuri);
+        }
+    }
+
+    function mintCopyToMultipleAddress(address[] memory _accounts,//allow contract owner to mint copy of a new nft to multiple adresses
+    uint256 _id,
+    bytes memory data) public onlyOwner{
+        for(uint i = 0; i < _accounts.length; i++){
+            uint _amount = 1;
+            _mintWithoutURI(_accounts[i], _id, _amount, data);
+        }
     }
 
 }
